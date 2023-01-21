7bef85a41a51059207d7b5deec1d38458be381f9
midhunkraghavan <midhunkraghavan@gmail.com>
2023-01-21 18:21:20 +0530

    # merge sort

--- Learnings/Sort/mergeSort.js
+++ Learnings/Sort/mergeSort.js
@@ -1,9 +1,10 @@
-// Sort array of element using insertion sort.
+// Sort array of element using merge sort.
 
 function mergeSort(arr) {
 
 
-    
+
+
 }
 
 console.log(mergeSort([12, 8, -2, 2, 16, -6, 2,2,8, 0]))
