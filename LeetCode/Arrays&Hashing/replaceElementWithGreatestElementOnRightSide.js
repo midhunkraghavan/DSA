a0bdba7f45730701bc780327faf83846d5719697
midhunkraghavan <midhunkraghavan@gmail.com>
2023-02-10 19:43:04 +0530

    # Replace Elements with Greatest Element On Right Side

---
+++ LeetCode/Arrays&Hashing/replaceElementWithGreatestElementOnRightSide.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ *                 Replace Elements with Greatest Element on Right Side
+ * 
+ * 
+ * Given an array arr, replace every element in that array with the greatest element
+ * among the elements to its right, and replace the last element with -1.
+ * 
+ * Example 1:
+
+       Input: arr = [17,18,5,4,6,1]
+       Output: [18,6,6,6,1,-1]
+
+  Explanation: 
+
+- index 0 --> the greatest element to the right of index 0 is index 1 (18).
+- index 1 --> the greatest element to the right of index 1 is index 4 (6).
+- index 2 --> the greatest element to the right of index 2 is index 4 (6).
+- index 3 --> the greatest element to the right of index 3 is index 4 (6).
+- index 4 --> the greatest element to the right of index 4 is index 5 (1).
+- index 5 --> there are no elements to the right of index 5, so we put -1.
+
+  Example 2:
+
+      Input: arr = [400]
+      Output: [-1]
+
+Explanation: There are no elements to the right of index 0.
+
+ */
+
+
+/**
+*  Solution 1 - 
+*/
+
+var replaceElements = function (arr) {
+    console.log(arr);
+    if (arr.length == 1) return [-1]
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+};
+
+console.log(replaceElements([17, 18, 5, 4, 6, 1]))
+//console.log(replaceElements([400]))
